33999249f323b34136fa33dae31c16456f052d43
aletheia <bianchi.luca@gmail.com>
2020-05-03 00:58:32 +0200

    :bug: Fixed service names. refs #3
    Fixed service names in enums

--- src/generator/generatePolicyEnums.ts
+++ src/generator/generatePolicyEnums.ts
@@ -6,16 +6,22 @@ import camelCase = require('lodash.camelcase');
 import toUpper = require('lodash.toupper');
 import snakeCase = require('lodash.snakecase');
 import capitalize = require('lodash.capitalize');
+import replace = require('lodash.replace');
 import fetch from 'node-fetch';
 
 const POLICY_URL = 'https://awspolicygen.s3.amazonaws.com/js/policies.js';
 const POLICY_JSON = resolve(__dirname, '../generated/policies.json');
 const POLICY_ENUM_FILE = resolve(__dirname, '../generated/PolicyEnums.ts');
 const SERVICES_ENUM_FILE = resolve(__dirname, '../generated/ServiceEnums.ts');
+const SERVICES_ARN_ENUM_FILE = resolve(
+  __dirname,
+  '../generated/ServiceArnEnums.ts'
+);
 
 interface AWSServicePolicy {
   StringPrefix: string;
   Actions: string[];
+  ARNFormat: string;
 }
 
 const retrievePolicyList = async (logger: Logger) => {
@@ -28,17 +34,24 @@ const retrievePolicyList = async (logger: Logger) => {
   logger.info('Saving policy file.');
   writeFileSync(POLICY_JSON, JSON.stringify(policyJson, null, 4));
 
+  let serviceArnString = '';
+  serviceArnString += 'export enum ServiceArn {\n';
+
   let serviceEnumString = '';
   serviceEnumString += 'export enum Service {\n';
 
   let policyEnumString = '';
   foreach(policyJson.serviceMap, (val: AWSServicePolicy, key: string) => {
-    const service = capitalize(
-      camelCase(key.replace('Amazon ', '').replace('AWS ', ''))
+    const name = key.replace('Amazon ', '').replace('AWS ', '');
+    const service = capitalize(camelCase(name));
+    const serviceUpperCase = toUpper(
+      replace(replace(name, /-|\(|\)|\./g, ''), / +/g, '_')
     );
-
     const serviceString = val.StringPrefix;
     serviceEnumString += `  ${service} = '${serviceString}',\n`;
+
+    const arnString = val.ARNFormat;
+    serviceArnString += `  ${serviceUpperCase} = '${arnString}',\n`;
 
     policyEnumString += `export enum ${service} {\n`;
     const res = val.Actions.map((action: string) => {
@@ -48,12 +61,17 @@ const retrievePolicyList = async (logger: Logger) => {
     policyEnumString += res + '\n';
     policyEnumString += '}\n\n';
   });
+
   serviceEnumString += '}\n';
   logger.info('Generating TS file containing Supported IAM Services enum.');
   writeFileSync(SERVICES_ENUM_FILE, serviceEnumString);
 
   logger.info('Generating TS file containing AWS Service Policies enums.');
   writeFileSync(POLICY_ENUM_FILE, policyEnumString);
+
+  serviceArnString += '}\n';
+  logger.info('Generating TS file containing ServiceArn');
+  writeFileSync(SERVICES_ARN_ENUM_FILE, serviceArnString);
 };
 
 const logger = winston.createLogger({
