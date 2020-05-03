2160275eee0fa41f2dccf04daf9a25dc0406ad89
aletheia <bianchi.luca@gmail.com>
2020-05-03 17:44:56 +0200

    :recycle: Rewrite Generator. refs #6
    generatePolicy completely rewritten as a class, to improve testability and code reuse.
    constants pulled out from generator.
    actions generated using consistent notation (screaming kebab case)
    actions generated with service string in value
    more consistent names for generated files

---
+++ src/generator/PolicyGeneratorFactory.ts
@@ -0,0 +1,180 @@
+import {writeFileSync} from 'fs';
+import {resolve} from 'path';
+import winston, {Logger} from 'winston';
+import foreach = require('lodash.foreach');
+import camelCase = require('lodash.camelcase');
+import toUpper = require('lodash.toupper');
+import snakeCase = require('lodash.snakecase');
+import capitalize = require('lodash.capitalize');
+import replace = require('lodash.replace');
+import fetch from 'node-fetch';
+
+const POLICY_URL = 'https://awspolicygen.s3.amazonaws.com/js/policies.js';
+const POLICY_JSON = resolve(__dirname, '../generated/policies.json');
+const FILE_ACTIONS_TS = resolve(__dirname, '../generated/Actions.ts');
+const FILE_SERVICES_TS = resolve(__dirname, '../generated/Services.ts');
+const FILE_ARN_TEMPLATE_TS = resolve(__dirname, '../generated/ArnTemplates.ts');
+
+interface ServiceName {
+  plain: string;
+  upperCamelCase: string;
+  screamingSnakeCase: string;
+}
+interface AWSPolicyJson {
+  serviceMap: {
+    [key in keyof string]: AWSServicePolicy;
+  };
+}
+interface AWSServicePolicy {
+  StringPrefix: string;
+  Actions: string[];
+  ArnFormat: string;
+}
+
+class PolicyGeneratorFactory {
+  logger: Logger;
+  policyJson?: AWSPolicyJson;
+
+  constructor(logger: Logger) {
+    this.logger = logger;
+  }
+
+  async fetchPolicyDescriptor(descriptorUrl: string) {
+    this.logger.info(`Fetching IAM policy metadata from ${descriptorUrl} `);
+    const response = await fetch(descriptorUrl);
+    const policyJs = await response.text();
+    const policyJsonString = policyJs.substring(
+      'app.PolicyEditorConfig='.length
+    );
+    const policyJson = JSON.parse(policyJsonString);
+
+    this.logger.info('Saving policy file.');
+    writeFileSync(POLICY_JSON, JSON.stringify(policyJson, null, 4));
+
+    this.policyJson = policyJson;
+  }
+
+  parseServiceName(serviceName: string): ServiceName {
+    const plain = serviceName.replace('Amazon ', '').replace('AWS ', '');
+    const upperCamelCase = capitalize(camelCase(plain));
+    const screamingSnakeCase = toUpper(
+      replace(replace(plain, /-|\(|\)|\./g, ''), / +/g, '_')
+    );
+    return {plain, upperCamelCase, screamingSnakeCase};
+  }
+
+  generateContent(
+    contentGeneratorLoop: Function,
+    contentHeader = '',
+    contentFooter = ''
+  ) {
+    let content = '';
+    content += contentHeader;
+
+    foreach(
+      this.policyJson!.serviceMap,
+      (val: AWSServicePolicy, key: string) => {
+        const serviceName = this.parseServiceName(key);
+        const extractedValue = val;
+        content += contentGeneratorLoop(serviceName, extractedValue);
+      }
+    );
+
+    content += contentFooter;
+    return content;
+  }
+
+  generateServiceContent() {
+    const contentHeader = 'export enum Service {\n';
+    const contentGeneratorLoop = (
+      serviceName: ServiceName,
+      extractedValue: AWSServicePolicy
+    ) => {
+      return `  ${serviceName.screamingSnakeCase} = '${extractedValue.StringPrefix}',\n`;
+    };
+    const contentFooter = '}\n';
+
+    return this.generateContent(
+      contentGeneratorLoop,
+      contentHeader,
+      contentFooter
+    );
+  }
+
+  generateServiceArnContent() {
+    const contentHeader = 'export enum ServiceArnTemplates {\n';
+    const contentGeneratorLoop = (
+      serviceName: ServiceName,
+      extractedValue: AWSServicePolicy
+    ) => {
+      return `  ${serviceName.screamingSnakeCase} = '${extractedValue.ArnFormat}',\n`;
+    };
+    const contentFooter = '}\n';
+
+    return this.generateContent(
+      contentGeneratorLoop,
+      contentHeader,
+      contentFooter
+    );
+  }
+
+  generatePolicyActionsContent() {
+    const contentGeneratorLoop = (
+      serviceName: ServiceName,
+      extractedValue: AWSServicePolicy
+    ) => {
+      let content = `export enum ${serviceName.screamingSnakeCase} {\n`;
+      const serviceString = extractedValue.StringPrefix;
+      const actionsContent = extractedValue.Actions.map((action: string) => {
+        const actionName = toUpper(snakeCase(action));
+        return `  ${actionName} = '${serviceString}:${action}'`;
+      }).join(',\n');
+      content += actionsContent + ',\n}\n\n';
+      return content;
+    };
+
+    return this.generateContent(contentGeneratorLoop);
+  }
+
+  generateFiles(
+    servicesOutFileName: string,
+    actionsOutFileName: string,
+    arnTemplatesOutFileName: string
+  ) {
+    logger.info('Generating TS file containing Supported IAM Services enum.');
+    const servicesContent = this.generateServiceContent();
+    writeFileSync(servicesOutFileName, servicesContent);
+
+    logger.info('Generating TS file containing AWS Service Policies enums.');
+    const actionsContent = this.generatePolicyActionsContent();
+    writeFileSync(actionsOutFileName, actionsContent);
+
+    logger.info('Generating TS file containing ServiceArn');
+    const serviceArnTemplatesContent = this.generateServiceArnContent();
+    writeFileSync(arnTemplatesOutFileName, serviceArnTemplatesContent);
+  }
+}
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+  ],
+});
+
+const generator = new PolicyGeneratorFactory(logger);
+generator
+  .fetchPolicyDescriptor(POLICY_URL)
+  .then(() => {
+    generator.generateFiles(
+      FILE_SERVICES_TS,
+      FILE_ACTIONS_TS,
+      FILE_ARN_TEMPLATE_TS
+    );
+  })
+  .then(() =>
+    logger.info('library data built. Please import package and have fun!')
+  );
