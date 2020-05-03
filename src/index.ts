0252e3be96b4a942d849c91f09c061fd9da18357
aletheia <bianchi.luca@gmail.com>
2020-05-03 17:48:57 +0200

    :recycle: Uniform names. refs #6
    Uniform file names and imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import * as API from './generated/PolicyEnums';
-import {Service} from './generated/ServiceEnums';
+import * as Action from './generated/Actions';
+import {Service} from './generated/Services';
 import {PolicyGenerator} from './PolicyGenerator';
 import {PolicyStatementGenerator} from './PolicyStatementGenerator';
-export {API, Service, PolicyGenerator, PolicyStatementGenerator};
+export {Action, Service, PolicyGenerator, PolicyStatementGenerator};
