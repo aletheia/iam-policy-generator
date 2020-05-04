9e77b6a748d3bb05e769edb88bde3dfe6f510277
aletheia <bianchi.luca@gmail.com>
2020-05-04 12:46:54 +0200

    :rotating_light: Update examples for linting
    Linter warning fix

--- examples/cdk-lambda-function/test/cdk-lambda-function.test.ts
+++ examples/cdk-lambda-function/test/cdk-lambda-function.test.ts
@@ -1,13 +1,21 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
+import {expect as expectCDK, matchTemplate, MatchStyle} from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as CdkLambdaFunction from '../lib/cdk-lambda-function-stack';
 
 test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = new CdkLambdaFunction.CdkLambdaFunctionStack(app, 'MyTestStack');
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT))
+  const app = new cdk.App();
+  // WHEN
+  const stack = new CdkLambdaFunction.CdkLambdaFunctionStack(
+    app,
+    'MyTestStack'
+  );
+  // THEN
+  expectCDK(stack).to(
+    matchTemplate(
+      {
+        Resources: {},
+      },
+      MatchStyle.EXACT
+    )
+  );
 });
