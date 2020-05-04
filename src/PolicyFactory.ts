cc59d14e5a54e8f814663f8543bf0a67136d0d71
aletheia <bianchi.luca@gmail.com>
2020-05-04 11:47:21 +0200

    :art: Props default. refs #8
    Sets default in constructor

--- src/PolicyFactory.ts
+++ src/PolicyFactory.ts
@@ -15,11 +15,13 @@ export class PolicyFactory {
   protected actions?: string[];
 
   constructor(props?: PolicyFactoryProps) {
+    this.effect = Effect.ALLOW;
+    this.resources = [];
+    this.actions = [];
     if (props) {
       const {effect, resources, actions} = props;
-      this.effect = effect ? effect : Effect.ALLOW;
-      this.resources = resources || [];
-      this.actions = [];
+      this.effect = effect ? effect : this.effect;
+      this.resources = resources ? resources : this.resources;
       if (actions) {
         actions.forEach(action => {
           this.addAction(action);
