abe435392fae65d042fd7772f74a78df91ad8062
Alex Casalboni <alexsaxmib@gmail.com>
2020-05-05 17:59:01 +0000

    Implement action validation, with tests

--- src/PolicyFactory.ts
+++ src/PolicyFactory.ts
@@ -1,3 +1,5 @@
+import * as Action from './generated/Actions';
+
 export enum Effect {
   ALLOW = 'Allow',
   DENY = 'Deny',
@@ -23,9 +25,7 @@ export class PolicyFactory {
       this.effect = effect ? effect : this.effect;
       this.resources = resources ? resources : this.resources;
       if (actions) {
-        actions.forEach(action => {
-          this.addAction(action);
-        });
+        this.addActions(actions);
       }
     }
   }
@@ -47,15 +47,29 @@ export class PolicyFactory {
     return this;
   }
 
+  private isValidAction(action: string) {
+    return Object.values(Action).find(service => {
+      return Object.values(service).find(a => a === action);
+    });
+  }
+
   addAction(action: string) {
+    if (this.isValidAction(action)) {
+      return this.addActionRaw(action);
+    }
+    throw new Error(
+      `Invalid action: "${action}".` +
+        'If you need to include a wildcard, please use factory.addActionRaw(action).'
+    );
+  }
+
+  addActionRaw(action: string) {
     this.actions.push(action);
     return this;
   }
 
   addActions(actions: string[]) {
-    actions.forEach(action => {
-      this.addAction(action);
-    });
+    actions.forEach(action => this.addAction(action));
     return this;
   }
 
--- src/__test__/PolicyFactory.test.ts
+++ src/__test__/PolicyFactory.test.ts
@@ -1,5 +1,33 @@
+import * as Action from '../generated/Actions';
 import {PolicyFactory} from '../PolicyFactory';
 
 test('should be defined', () => {
   expect(PolicyFactory).toBeDefined();
+});
+
+test('should accept valid actions', () => {
+  const factory = new PolicyFactory();
+  factory.addAction(Action.S3.PUT_OBJECT);
+  factory.addAction(Action.KINESIS.DESCRIBE_STREAM);
+  factory.addAction(Action.EC2.START_INSTANCES);
+
+  const json = factory.buildPolicy();
+
+  expect(JSON.parse(json).actions.length).toBe(3);
+});
+
+test('should not accept invalid actions', () => {
+  const factory = new PolicyFactory();
+  expect(() => {
+    factory.addAction('invalidservice:invalidaction');
+  }).toThrow();
+});
+
+test('should accept potentially invalid actions with addActionRaw', () => {
+  const factory = new PolicyFactory();
+  factory.addActionRaw('s3:Get*');
+  factory.addActionRaw('dynamodb:Describe*');
+
+  const json = factory.buildPolicy();
+  expect(JSON.parse(json).actions.length).toBe(2);
 });
